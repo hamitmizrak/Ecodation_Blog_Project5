76044e29f74fc47ee7c462c401d679a5c45ae59b
hamitmizrak <hamitmizrak@gmail.com>
2022-12-04 13:39:58 +0300

    BaseEntity

--- src/main/java/com/hamitmizrak/data/entity/BaseEntity.java
+++ src/main/java/com/hamitmizrak/data/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.hamitmizrak.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -19,6 +21,8 @@ import java.util.Date;
 //super class
 @MappedSuperclass
 //auditing
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value={"created_date,update_date"},allowGetters = true)
 abstract  public class BaseEntity implements Serializable {
 
     //ID
