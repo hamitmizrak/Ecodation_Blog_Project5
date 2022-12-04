3e86c7ec197c28efba852b5328998bae425a6d25
hamitmizrak <hamitmizrak@gmail.com>
2022-12-04 11:54:30 +0300

    Bean

---
+++ src/main/java/com/hamitmizrak/bean/OpenApiConfiguration.java
@@ -0,0 +1,30 @@
+package com.hamitmizrak.bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    //swagger
+    @Bean
+    public OpenAPI openAPIMethod(){
+        return new OpenAPI().info( new Info()
+                .title("title bilgisi")
+                .description("description bilgisi")
+                .version("V1.0")
+                .contact(new Contact()
+                        .name("Hamit")
+                        .url("www.hamitmizrak.com.tr")
+                        .email("hamitmizrak@gmail.com"))
+                .termsOfService(" Software INC.")
+                .license(new License()
+                        .name("Licence ")
+                        .url("www."))
+        );
+    }
+}
