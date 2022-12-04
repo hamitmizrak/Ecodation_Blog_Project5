d6a1fa48bdd3ba28c4eb87a3ddde940cd44a0a95
hamitmizrak <hamitmizrak@gmail.com>
2022-12-04 13:02:39 +0300

    Auditing

--- src/main/java/com/hamitmizrak/audit/AuditorAwareImpl.java
+++ src/main/java/com/hamitmizrak/audit/AuditorAwareImpl.java
@@ -1,14 +1,23 @@
 package com.hamitmizrak.audit;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+//@Component: işaretlediği nesneyi Spring'in bir nesnesi olmasını sağlıyor
+@Component
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     //sisteme girmiş login kişisi
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("HamitMızrak");
+        //org.springframework.security.core.Authentication;
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        //Eğer sistemde birisi varsa o kişinin ismini eklesin yoksa null döndürsün
+        return Optional.ofNullable(authentication!=null?authentication.getName():null);
+        //return Optional.of("HamitMızrak");
     }
 }
