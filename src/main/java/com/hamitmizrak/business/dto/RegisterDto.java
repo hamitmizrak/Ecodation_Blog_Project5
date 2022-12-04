2c853987336a598a1689b40378c61141453d3b34
hamitmizrak <hamitmizrak@gmail.com>
2022-12-04 11:02:42 +0300

    Validation

--- src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
+++ src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
@@ -19,15 +19,16 @@ import java.util.Date;
 public class RegisterDto {
     private Long id;
 
-    @NotNull(message = "{blog.username.validation.constraints.NotNull.message}")
+    @NotNull(message = "{register.username.validation.constraints.NotNull.message}")
     private String username;
 
-    @NotNull(message = "{blog.email.validation.constraints.NotNull.message}")
+    @NotNull(message = "{register.email.validation.constraints.NotNull.message}")
     @Email
     @Size(max = 200)
+    //my special annotation will created
     private String email;
 
-    @NotNull(message = "blog.password.validation.constraints.NotNull.message")
+    @NotNull(message = "{register.password.validation.constraints.NotNull.message}")
     @Size(min = 7,max = 20)
     // Hm123456@
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).*$",message = "{blog.password.validation.constraints.pattern.regex.message}")
