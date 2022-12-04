9ca1b919a45ec6c5e9ebf4609c774be4ae0be95b
hamitmizrak <hamitmizrak@gmail.com>
2022-12-04 10:18:50 +0300

    config

--- src/main/java/com/hamitmizrak/EcodationBlogProject5Application.java
+++ src/main/java/com/hamitmizrak/EcodationBlogProject5Application.java
@@ -1,13 +1,32 @@
 package com.hamitmizrak;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class
+}
+)
+//@SpringBootApplication
 public class EcodationBlogProject5Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcodationBlogProject5Application.class, args);
-	}
+    //Spring Constructor
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
 
+    public static void main(String[] args) {
+        //devtools active isActive
+        //System.setProperty("spring.devtools.restart.enabled","true");
+
+        //Disables headless
+        System.setProperty("java.awt.headless", "false");
+        SpringApplication.run(EcodationBlogProject5Application.class, args);
+    }
 }
