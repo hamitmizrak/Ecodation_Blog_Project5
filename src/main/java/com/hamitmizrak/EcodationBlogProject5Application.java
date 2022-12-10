9950229b02c400d8e3beca08b312015d8f556149
hamitmizrak <hamitmizrak@gmail.com>
2022-12-10 10:09:14 +0300

    @EnableJpaRepositories

--- src/main/java/com/hamitmizrak/EcodationBlogProject5Application.java
+++ src/main/java/com/hamitmizrak/EcodationBlogProject5Application.java
@@ -5,20 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
 
 
 //auditor: sisteme kim ne zaman veri ekledi veya güncelledi
-//audit: çalışması için enable yapmalıyız
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
 
 //Spring security Exclude
-@SpringBootApplication(exclude = {
-        SecurityAutoConfiguration.class
-}
-)
-//@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
+//@EnableJpaRepositories
+//Jpa Repository okumadığı  durumda @EnableJpaRepositories vermemiz gerekecek
+//D:\spring\EcodationBlogProject5\src\main\java\  com\hamitmizrak\data\repository
+@EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository")
+
 public class EcodationBlogProject5Application {
 
     //Spring Constructor
