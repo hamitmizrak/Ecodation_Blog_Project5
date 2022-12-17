6762cea19eb56d663b3a0228f8d3f32c03dfbafb
hamitmizrak <hamitmizrak@gmail.com>
2022-12-17 09:46:06 +0300

    Dockerize

--- src/main/java/com/hamitmizrak/EcodationBlogProject5Application.java
+++ src/main/java/com/hamitmizrak/EcodationBlogProject5Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PostConstruct;
